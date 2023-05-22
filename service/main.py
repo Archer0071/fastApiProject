1e6bdc224eb54ccbf0a4bfb7b0d67db1f922f6d5
archer <adilking0071@gmail.com>
2023-05-23 00:52:09 +0500

    removed hello

--- service/main.py
+++ service/main.py
@@ -8,12 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"message": "WELCOME TO FAST-API DEMO"}
 
 
 @app.get('/api/movie/{title}', response_model=MovieModel)
